ef4f95e12c8cc8f31fc593ab52f306e9f3388a61
Dmitry Shechtman <damnian@gmail.com>
2017-07-16 10:48:29 +0300

    Add Tests

---
+++ src/Net.Chdk.Meta.Generators.Platform.Eos.Tests/EosPlatformGeneratorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Net.Chdk.Meta.Generators.Platform.Eos.Tests
+{
+    [TestClass]
+    public class EosPlatformGeneratorTest
+    {
+        public EosPlatformGeneratorTest()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddPlatformGenerator()
+                .AddEosPlatformGenerator()
+                .BuildServiceProvider();
+            platformGenerator = serviceProvider.GetService<IPlatformGenerator>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullModels()
+        {
+            var result = platformGenerator.GetPlatform(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestEmptyModels()
+        {
+            var result = platformGenerator.GetPlatform(new string[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestNullModel()
+        {
+            var result = platformGenerator.GetPlatform(new string[] { null });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestEmptyModel()
+        {
+            var result = platformGenerator.GetPlatform(new[] { string.Empty });
+        }
+    }
+}
---
+++ src/Net.Chdk.Meta.Generators.Platform.Eos.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Net.Chdk.Meta.Generators.Platform.Eos.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Net.Chdk.Meta.Generators.Platform.Eos.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ccfb93a-51ab-4d83-aad9-85f535350069")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
